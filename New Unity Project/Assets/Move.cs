10c1831fa8e0d34b13dac5c04ba41911ae0dc7c3
Can Solak <cansolak@live.de>
2020-01-11 16:59:48 +0100

    Initial commit

---
+++ New Unity Project/Assets/Move.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float speed;
+    public float slowDownFactor = 0.85f;
+    public static string slowLog;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.left * speed * Time.deltaTime;
+    }
+
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Time.timeScale = slowDownFactor;
+        Time.fixedDeltaTime = 0.02f * Time.timeScale;
+        slowLog = Time.timeScale.ToString();
+        print(slowLog);
+    }
+
+}
